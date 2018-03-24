9bb2cff9b374f1c2fc4c98db2334534b91380347
florianvoelkers <florian.voelkers@mail.de>
2018-03-24 18:57:00 +0100

    finder fertig

--- Assets/Scripts/Smartphone/Finder.cs
+++ Assets/Scripts/Smartphone/Finder.cs
@@ -5,35 +5,78 @@ using UnityEngine.UI;
 
 public class Finder : MonoBehaviour {
 
+	public GameObject applications;
 	public GameObject profilePanel;
 	public GameObject welcomePanel;
+	public GameObject endPanel;
 	public Image profilePicture;
 	public Text nameAndAge;
 	public Text description;
-	public Image like;
-	public Image dislike;
+	public GameObject liked;
+	public GameObject disliked;
 	public Sprite [] profilePictures;
 	public string [] namesAndAges;
 	public string [] descriptions;
-
+	private int currentProfile;
 	private bool profilesActive;
 
 
 	void Update () {
-		if (profilesActive) {
+		if (profilesActive && Input.GetButtonDown ("Horizontal")) {
 			float moveX = Input.GetAxis ("Horizontal");
 			if (moveX < 0) {
-				Debug.Log ("dislike");
+				ShowDislikedSprite ();
 			} else if (moveX > 0) {
-				Debug.Log ("like");
+				ShowLikedSprite ();
 			}
+		}
+
+		if (Input.GetButtonDown ("Cancel")) {
+			applications.SetActive (true);
+			this.gameObject.SetActive (false);
+		}
+	}
+
+	private void ShowLikedSprite () {
+		liked.SetActive (true);
+		profilesActive = false;
+		StartCoroutine (DisplaySpriteForThreeSeconds ());
+	}
+
+	private void ShowDislikedSprite () {
+		disliked.SetActive (true);
+		profilesActive = false;
+		StartCoroutine (DisplaySpriteForThreeSeconds ());
+	}
+
+	private void ShowNextProfile () {
+		currentProfile++;
+		if (currentProfile < descriptions.Length) {
+			profilePicture.sprite = profilePictures [currentProfile];
+			nameAndAge.text = namesAndAges [currentProfile];
+			description.text = descriptions [currentProfile];
+			profilesActive = true;
+		} else {
+			profilePanel.SetActive (false);
+			endPanel.SetActive (true);
 		}
 	}
 
 	public void StartApplication () {
+		if (currentProfile >= descriptions.Length) {
+			endPanel.SetActive (true);
+			return;
+		}
 		profilesActive = false;
 		welcomePanel.SetActive (true);
 		StartCoroutine (WaitForMatches ());
+	}
+		
+	IEnumerator DisplaySpriteForThreeSeconds (){
+		yield return new WaitForSeconds (3f);
+		liked.SetActive (false);
+		disliked.SetActive (false);
+		ShowNextProfile ();
 	}
 
 	IEnumerator WaitForMatches () {
