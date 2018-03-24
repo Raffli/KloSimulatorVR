85989cf21aaff11643bb380fceee9c3e5ac64d25
Nikolai Stecker <Nikolai.Stecker@gmx.de>
2018-03-24 01:15:38 +0100

    vr samples importiert und spracherkennung eingebaut

---
+++ Assets/SprachAusgabe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SprachAusgabe : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private string words;
+
+
+    void Play()
+    {
+    }
+
+    public void SetWordsToSay(string words) {
+        this.words = words;
+        Play();
+    } 
+
+
+}
