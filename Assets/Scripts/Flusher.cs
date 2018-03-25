d4764000b7cd98e230e434d58ea383cd5d91f705
Wavy <wavy1s@mail.ru>
2018-03-25 03:05:25 +0200

    Flushing scripts changed

---
+++ Assets/Scripts/Flusher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flusher : MonoBehaviour {
+
+    public GameObject toiletteWater;
+
+    public void initFlush()
+    {
+        toiletteWater.GetComponent<FlushingWater>().flush();
+    }
+}
